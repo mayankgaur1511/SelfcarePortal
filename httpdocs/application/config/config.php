f33c07b6f04bb368521c790bedb4769d1a6b5c37
mayankgaurArkadin <35913587+mayankgaurArkadin@users.noreply.github.com>
2018-04-26 12:20:07 +0530

    fixed syntax error

--- httpdocs/application/config/config.php
+++ httpdocs/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-if(ISSET($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST'])}}{}
+if(ISSET($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST'])
 {
     $config['base_url'] =  "http://" . $_SERVER['HTTP_HOST'];
 }
